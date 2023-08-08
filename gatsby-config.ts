b75f98b31a829d3c29fc8f88fd15e3c664199495
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-08-08 13:35:06 -0700

    :sparkles: content added + blog page rough

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,7 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Ryosuke's Blog`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://whoisryosuke.com`
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
@@ -17,10 +17,10 @@ const config: GatsbyConfig = {
   }, "gatsby-plugin-mdx", {
     resolve: 'gatsby-source-filesystem',
     options: {
-      "name": "pages",
-      "path": "./src/pages/"
+      "name": "blog",
+      "path": "./content/blog/"
     },
-    __key: "pages"
+    __key: "blog"
   }]
 };
 
---
+++ gatsby-node.js
@@ -0,0 +1,62 @@
+const path = require("path");
+const { createFilePath } = require(`gatsby-source-filesystem`);
+const blogPostTemplate = path.resolve(`./src/templates/blog-post.tsx`);
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions;
+
+  if (node.internal.type === "Mdx") {
+    const value = createFilePath({ node, getNode, basePath: `content` });
+    createNodeField({
+      // Name of the field you are adding
+      name: "slug",
+      // Individual MDX node
+      node,
+      // Generated value based on filepath with "blog" prefix. We
+      // don't need a separating "/" before the value because
+      // createFilePath returns a path with the leading "/".
+      value: `/${node.frontmatter.section}${value}`,
+    });
+  }
+};
+
+exports.createPages = async ({ graphql, actions, reporter }) => {
+  const { createPage } = actions;
+
+  const result = await graphql(`
+    query {
+      allMdx {
+        nodes {
+          id
+          fields {
+            slug
+          }
+          internal {
+            contentFilePath
+          }
+        }
+      }
+    }
+  `);
+
+  if (result.errors) {
+    reporter.panicOnBuild("Error loading MDX result", result.errors);
+  }
+
+  // Create blog post pages.
+  const posts = result.data.allMdx.nodes;
+
+  // you'll call `createPage` for each result
+  posts.forEach((node) => {
+    createPage({
+      // As mentioned above you could also query something else like frontmatter.title above and use a helper function
+      // like slugify to create a slug
+      path: node.fields.slug,
+      // Provide the path to the MDX content file so webpack can pick it up and transform it into JSX
+      component: `${blogPostTemplate}?__contentFilePath=${node.internal.contentFilePath}`,
+      // You can use the values in this context in
+      // our page layout component
+      context: { id: node.id },
+    });
+  });
+};
